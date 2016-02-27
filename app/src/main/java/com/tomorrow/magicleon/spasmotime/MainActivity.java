40ab1a7e6ad18453463dc80b7ca39a6e10b0178d
magicleon <magicleon94@gmail.com>
2016-02-27 21:03:04 +0100

    aggiunta persistenza e fixato l'intent filter

--- app/src/main/java/com/tomorrow/magicleon/spasmotime/MainActivity.java
+++ app/src/main/java/com/tomorrow/magicleon/spasmotime/MainActivity.java
@@ -1,6 +1,8 @@
 package com.tomorrow.magicleon.spasmotime;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -14,14 +16,23 @@ public class MainActivity extends AppCompatActivity {
     Switch palindromi;
     Switch simmetrici;
     Button startButton;
-
+    SharedPreferences sharedPreferences;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //inizializzo i riferimenti ai componenti
         startButton = (Button) findViewById(R.id.saveButton);
         palindromi = (Switch) findViewById(R.id.palindromsSwitch);
         simmetrici = (Switch) findViewById(R.id.simmetricsSwitch);
+
+        //mi prendo le sharedPreferences
+        sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+
+        //carico i valori di checked precedentemente impostati
+        palindromi.setChecked(sharedPreferences.getBoolean(getString(R.string.sharedPreferencePalindromEnabledKey),false));
+        simmetrici.setChecked(sharedPreferences.getBoolean(getString(R.string.sharedPreferenceSymmetricEnabledKey),false));
 
         startButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -31,31 +42,6 @@ public class MainActivity extends AppCompatActivity {
                 intent.putExtra(Constants.SWITCH_PALINDROMS_EXTRA,palindromi.isChecked());
                 intent.putExtra(Constants.SWITCH_SYMMETRIC_EXTRA,simmetrici.isChecked());
                 startService(intent);
-
-
-
-//              int firstId = atomicInteger.incrementAndGet();
-//              int secondId = atomicInteger.incrementAndGet();
-//              intent.putExtra("intentTime","12:21");
-//              firstPendingIntent = PendingIntent.getBroadcast(getApplicationContext(),firstId,intent,0);
-//              secondPendingIntent = PendingIntent.getBroadcast(getApplicationContext(),secondId,intent,0);
-//              Calendar firstLullo = Calendar.getInstance();
-//              Calendar secondLullo = Calendar.getInstance();
-//              firstLullo.set(Calendar.HOUR_OF_DAY,12);
-//              firstLullo.set(Calendar.MINUTE,35);
-//              firstLullo.set(Calendar.SECOND,0);
-//              firstLullo.set(Calendar.MILLISECOND,0);
-//              secondLullo.set(Calendar.HOUR_OF_DAY,12);
-//              secondLullo.set(Calendar.MINUTE,36);
-//              secondLullo.set(Calendar.SECOND,0);
-//              secondLullo.set(Calendar.MILLISECOND,0);
-//              if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
-//                  alarmManager.setExact(AlarmManager.RTC_WAKEUP,firstLullo.getTimeInMillis(),firstPendingIntent);
-//                  alarmManager.setExact(AlarmManager.RTC_WAKEUP,secondLullo.getTimeInMillis(),secondPendingIntent);
-//              }
-//              alarmManager.setExact(AlarmManager.RTC_WAKEUP, System.currentTimeMillis()+3000,firstPendingIntent);
-//              Log.d("ALARM","settato con id " + String.valueOf(firstId));
-//              Log.d("ALARM","settato con id " + String.valueOf(secondId));
             }
         });
     }
--- app/src/main/java/com/tomorrow/magicleon/spasmotime/updateAlarmsService.java
+++ app/src/main/java/com/tomorrow/magicleon/spasmotime/updateAlarmsService.java
@@ -4,6 +4,8 @@ import android.app.AlarmManager;
 import android.app.IntentService;
 import android.app.PendingIntent;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.util.Log;
 
 import java.util.Calendar;
@@ -39,14 +41,17 @@ import java.util.concurrent.atomic.AtomicInteger;
         }
         if(simmetrici){
             Calendar palindrotime = Calendar.getInstance();
+            AlarmManager alarmManager = ((AlarmManager) getApplicationContext().getSystemService(ALARM_SERVICE));
             palindrotime.set(Calendar.HOUR,0);
             palindrotime.set(Calendar.MINUTE,0);
             palindrotime.set(Calendar.SECOND,0);
 
             Intent palindroIntent = new Intent(Constants.ALARM_ACTION_PALINDROM);
             PendingIntent pendingIntent = PendingIntent.getBroadcast(getApplicationContext(),id.incrementAndGet(),intent,0);
+
+            alarmManager.cancel(pendingIntent);
             // setto la ripetizione a 61 minuti * 60 secondi * 1000 millisecondi
-            ((AlarmManager) getApplicationContext().getSystemService(ALARM_SERVICE)).setRepeating(AlarmManager.RTC_WAKEUP,palindrotime.getTimeInMillis(),61*60*1000,pendingIntent);
+            alarmManager.setRepeating(AlarmManager.RTC_WAKEUP,palindrotime.getTimeInMillis(),61*60*1000,pendingIntent);
 
 
             //TODO setta tutti gli allarmi dei simmetrici
@@ -59,6 +64,13 @@ import java.util.concurrent.atomic.AtomicInteger;
             //22:22
             //23:23
         }
+
+        //una volta finito il lavoro salvo i nuovi valori
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putBoolean(getString(R.string.sharedPreferencePalindromEnabledKey),palindromi);
+        editor.putBoolean(getString(R.string.sharedPreferenceSymmetricEnabledKey),simmetrici);
+        editor.commit();
     }
     public void onDestroy() {
         super.onDestroy();
